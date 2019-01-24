f2a899b83e251673bbbfe4444139e241c14e137a
Baruch Hashem <barukhashem@gmail.com>
2019-01-24 00:00:03 -0700

    latest

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,11 @@
 // PSEUDOCODING:
 // This is a timed game with trivia questions and multiple choice answers.
 
-// The player can only select one answer per question.
+// The player can only select one answer per question (via radio buttons).
 
 // The game ends when the player either finishes early or the time runs out. The results page will then tally and display the number of the player's correctly and incorrectly answered questions.
 
 // Clicking the Start button begins the game with the countdown timer counting down.
-
-// Create a "Start" button for the countdown timer.
-
-// Create an "I'm Finished!" button if player finishes early.
 
 // Create questions and answers.
 
@@ -18,7 +14,7 @@
 // The game should restart if the player clicks Start.
 
 // Create a countdown timer that counts down 60 seconds:
-var seconds = 5;
+var seconds = 10;
 
 var intervalId;
 
@@ -56,29 +52,44 @@ function stop() {
 // // Creates Finished button on-click listener event:
 // $("#submit").on("click", submit);
 
-function submit() {
-    var correct = 0;
-    var numQuestions = 5;
-    var ansArr = ["a", "b", "c", "d", "d"];
+// function submit() {
+//     var correct = 0;
+//     var numQuestions = 5;
+//     var ansArr = ["a", "b", "c", "d", "d"];
 
-    var q1 = document.forms["trivia-game"]["q1"].value;
-    var q2 = document.forms["trivia-game"]["q2"].value;
-    var q3 = document.forms["trivia-game"]["q3"].value;
-    var q4 = document.forms["trivia-game"]["q4"].value;
-    var q5 = document.forms["trivia-game"]["q5"].value;
+//     var q1 = document.forms["trivia-game"]["q1"].value;
+//     var q2 = document.forms["trivia-game"]["q2"].value;
+//     var q3 = document.forms["trivia-game"]["q3"].value;
+//     var q4 = document.forms["trivia-game"]["q4"].value;
+//     var q5 = document.forms["trivia-game"]["q5"].value;
 
-    for (var i = 0; i < numQuestions.length; i++) {
-        var response = window.prompt(questions[i].prompt);
+//     for (var i = 1; i <= numQuestions.length; i++) {
+//         if (eval("q" + i) === "") {
+//             console.log("Wrong!");
+//         }
 
-        if (response === questions[i].answer) {
-            correct++;
-            console.log("Correct!");
-        } else {
-            console.log("Wrong!");
-        }
-    }
-};
-    console.log("You got " + correct + "/" + questions.length + " correct.");
+
+//         for (var i = 1; i <= numQuestions.length; i++) {
+//             if (eval("q" + i) === ansArr[i - 1]) {
+//                 correct++;
+//             }
+//         }
+
+//         var results = document.getElementById("results")
+//         results.innerHTML = "<h2>You scored " + score + "/" + numQuestions + "</h2>"
+
+//     for (var i = 0; i < numQuestions.length; i++) {
+//         var response = window.prompt(questions[i].prompt);
+
+//         if (response === questions[i].answer) {
+//             correct++;
+//             console.log("Correct!");
+//         } else {
+//             console.log("Wrong!");
+//         }
+//     }
+// };
+//     console.log("You got " + correct + "/" + questions.length + " correct.");
 
 
 // var questions = [
